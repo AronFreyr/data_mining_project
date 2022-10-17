3116768d14f14fd8bcddd853772b8030b53a15e3
aronf <aronfreyrh@gmail.com>
2022-10-17 21:47:16 +0200

    Added tests that do the same things for both GMM and RGMM

--- tests/test_gmm.py
+++ tests/test_gmm.py
@@ -1,6 +1,9 @@
 import unittest
 
 import gmm
+import generate_data
+import generate_graphs
+from sklearn.datasets import make_moons, make_circles, make_blobs
 
 
 class TestGMM(unittest.TestCase):
@@ -8,9 +11,91 @@ class TestGMM(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_plot_data(self):
-        x = gmm.generate_data_for_gmm(show_plot=False)
-        self.assertEqual(500, len(x))
+    def test_get_plots_for_data_example_1(self):
+        dimensions = 2
+        clusters = 2
+        test_gmm = gmm.GMM(clusters)
+        x = generate_data.data_example_1(show_plot=False)
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_for_data_example_2(self):
+        dimensions = 2
+        clusters = 4
+        test_gmm = gmm.GMM(clusters)
+        x = generate_data.data_example_2(show_plot=False)
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+        #generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_for_data_example_3(self):
+        dimensions = 2
+        clusters = 4
+        test_gmm = gmm.GMM(clusters)
+        x = generate_data.data_example_3(show_plot=False)
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_for_data_example_5(self):
+        dimensions = 2
+        clusters = 5
+        test_gmm = gmm.GMM(clusters)
+        x = generate_data.data_example_5(show_plot=False)
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_for_data_example_6(self):
+        dimensions = 2
+        clusters = 16
+        test_gmm = gmm.GMM(clusters, eps=0.001)
+        x = generate_data.data_example_6(show_plot=False)
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_for_data_example_moons(self):
+        dimensions = 2
+        clusters = 2
+        test_gmm = gmm.GMM(clusters)
+        #x = generate_data.data_example_9(show_plot=False)
+        x = make_moons(n_samples=1000)[0]
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+        circles = make_circles(n_samples=1000, factor=0.5, noise=0.05)
+
+    def test_get_plots_fro_data_example_iris(self):
+        dimensions = 2
+        clusters = 2
+        test_gmm = gmm.GMM(clusters, eps=0.0001)
+        # x = generate_data.data_example_9(show_plot=False)
+        x = make_circles(n_samples=1000, factor=0.5, noise=0.05)[0]
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
+
+    def test_get_plots_fro_data_example_7(self):
+        dimensions = 3
+        clusters = 9
+        test_gmm = gmm.GMM(clusters, eps=0.0001)
+        x = generate_data.data_example_7(show_plot=False)
+        #x = make_circles(n_samples=1000, factor=0.5, noise=0.05)[0]
+        test_gmm.fit(x)
+        prediction = test_gmm.predict(x)
+        test_gmm.make_clusters()
+        generate_graphs.plot_predictions(dimensions, x, clusters, prediction, plot_title='Non-robust GMM')
 
 
 if __name__ == '__main__':
--- tests/test_robust_gmm.py
+++ tests/test_robust_gmm.py
@@ -3,6 +3,7 @@ import unittest
 import robust_gmm
 import generate_data
 import generate_graphs
+from sklearn.datasets import make_moons, make_circles, make_blobs
 
 
 class TestRobustGMM(unittest.TestCase):
@@ -11,24 +12,69 @@ class TestRobustGMM(unittest.TestCase):
         pass
 
     def test_run_robust_gmm(self):
-        x = generate_data.data_example_5(show_plot=False)
+        x = generate_data.data_example_2(show_plot=False)
         gmm = robust_gmm.RobustGMM(eps=0.001, plot_intermediate_steps=True, plot_step_counter=5)
         gmm.fit(x)
         predictions = gmm.predict(x)
-        # print(predictions)
         gmm.make_clusters()
         gmm.plot_predictions()
-        # generate_graphs.plot_predictions(gmm.d, x, gmm.c, gmm.clusters)
+
+    def test_get_plots_for_data_example_1(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = generate_data.data_example_1(show_plot=False)
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_2(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = generate_data.data_example_2(show_plot=False)
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_3(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = generate_data.data_example_3(show_plot=False)
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_5(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = generate_data.data_example_5(show_plot=False)
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_6(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = generate_data.data_example_6(show_plot=False)
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_moons(self):
+        test_gmm = robust_gmm.RobustGMM()
+        x = make_moons(n_samples=1000)[0]
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
+
+    def test_get_plots_for_data_example_iris(self):
+        test_gmm = robust_gmm.RobustGMM(plot_intermediate_steps=True, plot_step_counter=5)
+        x = make_circles(n_samples=1000, factor=0.5, noise=0.05)[0]
+        test_gmm.fit(x)
+        test_gmm.make_clusters()
+        test_gmm.plot_predictions()
 
     def test_run_robust_gmm_with_3d_data(self):
-        x = generate_data.data_example_1_3D(show_plot=False)
+        x = generate_data.data_example_7(show_plot=False)
         gmm = robust_gmm.RobustGMM()
         gmm.fit(x)
         predictions = gmm.predict(x)
-        # print(predictions)
         gmm.make_clusters()
         gmm.plot_predictions()
-        #generate_graphs.plot_predictions(gmm.d, x, gmm.c, gmm.clusters)
 
 
 if __name__ == '__main__':
